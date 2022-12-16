330907966e4d9cfb46d2e591839ba821d696d64f
邓澎波 <dengpbs@163.com>
2022-12-16 18:02:11 +0800

    第一次提交代码

---
+++ src/components/common/Header.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="header-container">
+    <div class="l-content">
+      <el-button
+        icon="el-icon-menu"
+        size="medium"
+        style="margin-left: 20px"
+        @click="handleMenu"
+      ></el-button>
+      <el-breadcrumb separator="/" style="padding-left:20px;">
+        <el-breadcrumb-item v-for="item in tags" :key="item.path" :to="{ path: item.path }">
+          {{item.label}}
+        </el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
+    <div class="r-content">
+      <el-dropdown >
+        <span class="el-dropdown-link">
+          <img class="user_img" src="@/assets/images/user1.png" />
+        </span>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item  >个人信息</el-dropdown-item>
+          <el-dropdown-item  >退出</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </div>
+  </div>
+</template>
+<script>
+import {mapState} from 'vuex'
+export default {
+  name: "Header",
+  data() {
+    return {};
+  },
+  methods: {
+    handleMenu() {
+      //console.log(111)
+      // 对菜单做缩放操作
+      this.$store.commit("collapseChange");
+    }
+  },computed:{
+    ...mapState({
+      tags:state => state.tab.tabsList
+    })
+  }
+};
+</script>
+<style lang="less" scoped>
+.header-container {
+  height: 60px;
+  background-color: #242f42;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  .firstPage {
+    color: #fff;
+    font-size: 16px;
+    margin-left: 10px;
+    cursor: pointer;
+  }
+  .r-content {
+    padding-right: 20px;
+    .user_img {
+      width: 40px;
+      height: 40px;
+      border-radius: 50%;
+    }
+  }
+  .l-content{
+    display: flex;
+    align-items: center;
+    /deep/.el-breadcrumb__item{
+        .el-breadcrumb__inner{
+            font-weight: normal;
+            &.is-link{
+                color:#666;
+            }
+        }
+        &:last-child{
+            .el-breadcrumb__inner{
+                color:#fff;
+            }
+        }
+
+    }
+  }
+}
+
+</style>
